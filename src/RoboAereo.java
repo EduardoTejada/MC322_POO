2bdc367ef2457b09f56a35211b15aea97e86dde7
daMattaPedro <pedro.bmatta@gmail.com>
2025-04-28 22:37:38 -0300

    implementação sensor temp

--- src/RoboAereo.java
+++ src/RoboAereo.java
@@ -1,8 +1,5 @@
 // Classe RoboAereo que herda da classe Robo
 public class RoboAereo extends Robo {
-    // Altitude atual do robô
-    private int altitude;
-    
     // Altitude máxima permitida para o robô
     private int altitudeMaxima;
 
@@ -10,16 +7,16 @@ public class RoboAereo extends Robo {
     // Inicializa o robô com nome, direção, posição (x, y), altitude e altitude máxima
     public RoboAereo(String n, String d, int x, int y, Ambiente amb, int alt, int altMax){
         super(n, d, x, y, amb); // Chama o construtor da classe pai (Robo)
-        altitude = alt; // Define a altitude inicial do robô
+        this.setAltitude(alt); // Define a altitude inicial do robô
         altitudeMaxima = altMax; // Define a altitude máxima permitida
     }
     
     // Método para subir o robô em uma determinada altitude (deltaZ)
     public void subir(int deltaZ){
-        if (altitude + deltaZ <= altitudeMaxima) {
-            altitude += deltaZ;
+        if (this.getAltitude() + deltaZ <= altitudeMaxima) {
+            this.setAltitude(this.getAltitude()+ deltaZ);
         } else {
-            altitude = altitudeMaxima;
+            this.setAltitude(altitudeMaxima);
             System.out.println("Altura máxima atingida!");
         }
     }
@@ -27,23 +24,12 @@ public class RoboAereo extends Robo {
 
     // Método para descer o robô em uma determinada altitude (deltaZ)
     public void descer(int deltaZ){
-        if (altitude - deltaZ >= 0) {
-            altitude -= deltaZ;
+        if (this.getAltitude() - deltaZ >= 0) {
+            this.setAltitude(this.getAltitude()- deltaZ);
         } else {
-            altitude = 0;
+            this.setAltitude(0);
             System.out.println("O robô já está no chão!");
         }
-    }
-    
-
-    // Retorna a altitude atual do robô
-    public int getAltitude() {
-        return altitude;
-    }
-
-    // Define um novo valor para a altitude do robô
-    public void setAltitude(int altitude) {
-        this.altitude = altitude;
     }
 
     // Retorna a altitude máxima permitida para o robô
--- src/SensorTemp.java
+++ src/SensorTemp.java
@@ -10,10 +10,16 @@ public class SensorTemp extends Sensor {
     public void monitorar(Ambiente amb) {
         //Detctando outros Robôs
         for (Robo rb : amb.getRobos()){ //Verifica a lista de outros robôs no ambiente e retorna as coordenadas daqueles que estão dentro do raio do snessor
-            if(this.getRaio() >= Math.sqrt(Math.Pow(rb.getPosX() , 2)+Math.Pow(rb.getPosY , 2))){
-                System.out.println("Robô detectado: (" + rb.getPosX() + ", " + rb.getPosY() + ")");
+            if(this.getRaio() >= Math.sqrt(Math.pow(rb.getPosX(), 2) + Math.pow(rb.getPosY(), 2) + Math.pow(rb.getAltitude(), 2))){
+                System.out.println("*************************************************************************************");
+                System.out.println("Robô detectado: (" + rb.getPosX() + ", " + rb.getPosY() + ", " + rb.getAltitude() + ")");
+                System.out.println("Temperatura: (" + rb.getTemp()+ ")");
+                System.out.println("*************************************************************************************");
+
             }else{
+                System.out.println("*************************************************************************************");
                 System.out.println("Nenhum robô detectado:");
+                System.out.println("*************************************************************************************");
             }
         }
         
