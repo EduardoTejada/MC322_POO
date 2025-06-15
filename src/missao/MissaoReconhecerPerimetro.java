61e06fc8ae8f51db112ded48456d68cd13625485
daMattaPedro <pedro.bmatta@gmail.com>
2025-06-15 19:04:09 -0300

    criacao interface missao,duas missoes concretas classe abstrata robo inteligente e classe robo lunar

---
+++ src/missao/MissaoReconhecerPerimetro.java
@@ -0,0 +1,14 @@
+package missao;
+
+import ambiente.Ambiente;
+import robo.Robo;
+
+//verifica o perimetro e registra obstaculos detectados em uma lista
+public class MissaoReconhecerPerimetro implements Missao{
+
+    @Override
+    public void executar(Robo r, Ambiente a) {
+        
+    }
+    
+}
---
+++ src/robo/AgenteInteligente.java
@@ -0,0 +1,24 @@
+package robo;
+
+import ambiente.Ambiente;
+import missao.Missao;
+
+public abstract class AgenteInteligente extends Robo{
+    
+    public AgenteInteligente(String n, String d, int x, int y, Ambiente amb) {
+        super(n, d, x, y, amb);
+        //TODO Auto-generated constructor stub
+    }
+
+    protected missao.Missao missao;
+    
+    public void definirMissao(Missao m) {
+        this.missao = m;
+    }
+
+    public boolean temMissao () {
+        return missao != null;
+    }
+
+    public abstract void executarMissao(Ambiente a);
+}
